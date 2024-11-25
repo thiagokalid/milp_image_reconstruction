d1e1bec172cafc5ef0db9f402d0336d9c8981c8c
Thiago Kalid <thiagokkalid@gmail.com>
2024-11-25 17:38:02 -0300

    method based on IRLS

--- examples/main.py
+++ examples/main.py
@@ -8,6 +8,7 @@ from src.milp_image_reconstruction.imaging import *
 
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.use("TkAgg")
 import time
 
@@ -17,8 +18,9 @@ import time
 cp = 5.9  # Velocidade de propagação no meio em mm/us
 gate_start = 0  # Início do gate em us
 gate_end = 10  # Fim do gate em us
-fc = .25e6  # Frequência central do transdutor em Hz
-fs = fc * 5  # Frequência de amostragem em Hz
+fc = 1e6  # Frequência central do transdutor em Hz
+fs = fc * 4  # Frequência de amostragem em Hz
+Nelem = 32
 
 #%% Criação dos Objetos para Simulação:
 
@@ -26,17 +28,16 @@ fs = fc * 5  # Frequência de amostragem em Hz
 reflector_grid = ReflectorGrid(width=6, height=7, xres=.5, zres=.5)
 
 # Create transducer:
-transducer = Transducer(n_elem=32, fc=fc)
+transducer = Transducer(n_elem=Nelem, fc=fc)
 
 # Create acquisiton object:
 acq = Acquisition(cp, fs, gate_start, gate_end, reflector_grid, transducer)
 # %% Aplicação do método de reconstrução de imagem:
 
 # Localização do refletor que deseja-se reconstruir em mm:
-xr = [-1.25, 2.5, 0.75]
-zr = [3.4, 6, 3]
+xr = [-1.25]
+zr = [3]
 
-#
 sampled_fmc = None
 for xi, zi in zip(xr, zr):
     if sampled_fmc is None:
@@ -46,6 +47,7 @@ for xi, zi in zip(xr, zr):
 
 sampled_signal = np.ravel(sampled_fmc)
 signal_size = len(sampled_signal)
+sampled_signal += np.random.rand(signal_size) * 0.05
 
 #
 imgsize = reflector_grid.get_imgsize()
@@ -62,7 +64,7 @@ print("L1 end.")
 
 print("L2 begin.")
 t0 = time.time()
-img_reference, l2_residue = passarin_method(
+img_reference, l2_residue = irls_method(
     np.reshape(acq.fmc_basis, newshape=(signal_size, reflector_grid.get_numpxs())),
     sampled_signal,
     reflector_grid.get_imgsize()
@@ -74,16 +76,17 @@ print("L2 end.")
 #%% Display dos resultados:
 convert_to_db = lambda img: 20 * np.log10(img - img.min() / (img.max() - img.min()) + 1e-9)
 
-offset = (reflector_grid.xres/2, reflector_grid.zres/2)
+offset = (reflector_grid.xres / 2, reflector_grid.zres / 2)
 
 img_proposed_db = convert_to_db(img_proposed)
 img_reference_db = convert_to_db(img_reference)
 
 vmin = np.min([img_reference_db])
+vmax = np.max([img_reference_db])
 
-plt.subplot(2,2,1)
+plt.subplot(2, 2, 1)
 plt.suptitle("Image in dB")
-plt.imshow(img_proposed_db, extent=reflector_grid.get_extent(offset=offset), aspect='equal', vmin=vmin, vmax=-6.8)
+plt.imshow(img_proposed_db, extent=reflector_grid.get_extent(offset=offset), aspect='equal', vmin=vmin, vmax=vmax)
 plt.plot(*reflector_grid.get_coords(), "xb", alpha=.5, label="Reflectors grid")
 plt.plot(xr, zr, 'or', label='Target reflector')
 plt.xlabel("x-axis in mm")
@@ -92,11 +95,11 @@ plt.title(f"Runtime = {t_proposed:.2f} s")
 plt.colorbar()
 plt.legend()
 
-ax = plt.subplot(2,2,3)
+ax = plt.subplot(2, 2, 3)
 ax.hist(l1_residue, bins=100, density=False)
 
-plt.subplot(2,2,2)
-plt.imshow(img_reference_db, extent=reflector_grid.get_extent(offset=offset), aspect='equal', vmin=vmin, vmax=-6.8)
+plt.subplot(2, 2, 2)
+plt.imshow(img_reference_db, extent=reflector_grid.get_extent(offset=offset), aspect='equal', vmin=vmin, vmax=vmax)
 plt.plot(*reflector_grid.get_coords(), "xb", alpha=.5, label="Reflectors grid")
 plt.plot(xr, zr, 'or', label='Target reflector')
 plt.xlabel("x-axis in mm")
@@ -105,7 +108,7 @@ plt.title(f"Runtime = {t_reference:.2f} s")
 plt.colorbar()
 plt.legend()
 
-ax = plt.subplot(2,2,4)
+ax = plt.subplot(2, 2, 4)
 ax.hist(l2_residue, bins=100, density=False)
 
 plt.show()
--- src/milp_image_reconstruction/imaging.py
+++ src/milp_image_reconstruction/imaging.py
@@ -1,31 +1,36 @@
 import numpy as np
 import scipy
 import scipy.sparse.linalg as linalg
+from scipy.sparse.linalg import cg
 
 from .acquisition import Acquisition
 
 from scipy.optimize import milp
-def transform_dense_to_sparse_array(dense_signal, epsilon = 1e-2):
+
+
+def transform_dense_to_sparse_array(dense_signal, epsilon=1e-2):
     sparse_signal = np.zeros_like(dense_signal)
     non_zero = np.power(dense_signal, 2) > np.power(epsilon, 2)
     sparse_signal[non_zero] = dense_signal[non_zero]
     sparse_signal = scipy.sparse.csc_array(sparse_signal)
     return sparse_signal
 
-def transform_dense_to_sparse_matrix(dense_signal, epsilon = 1e-2):
+
+def transform_dense_to_sparse_matrix(dense_signal, epsilon=1e-2):
     sparse_signal = np.zeros_like(dense_signal)
     non_zero = np.power(dense_signal, 2) > np.power(epsilon, 2)
     sparse_signal[non_zero] = dense_signal[non_zero]
     sparse_signal = scipy.sparse.csc_array(sparse_signal)
     return sparse_signal
+
 
 def passarin_method(basis_signal: np.ndarray, sampled_signal: np.ndarray, imgsize: tuple, damp=0):
     A = basis_signal
     b = sampled_signal
     x = linalg.lsqr(A, b, damp=damp)[0]
-    img = np.reshape(x, newshape=imgsize)
-    residue = b - A@x
-    return img.T, b - A@x
+    img = np.reshape(x, shape=imgsize)
+    residue = b - A @ x
+    return img.T, b - A @ x
 
 
 def naive_l1_method(basis_signal: np.ndarray, sampled_signal: np.ndarray, imgsize: tuple):
@@ -38,7 +43,7 @@ def naive_l1_method(basis_signal: np.ndarray, sampled_signal: np.ndarray, imgsiz
 
     result = scipy.optimize.minimize(fun=cost_fun, x0=np.zeros(N), method="SLSQP")
 
-    img = np.reshape(result.x, newshape=imgsize)
+    img = np.reshape(result.x, shape=imgsize)
     return img.T
 
 
@@ -50,9 +55,8 @@ def l1_method(basis_signal: np.ndarray, sampled_signal: np.ndarray, imgsize: tup
     H = transform_dense_to_sparse_matrix(H)
 
     # c^T @ x
-    c = np.ones(shape=(2*N + M, 1))
+    c = np.ones(shape=(2 * N + M, 1))
     c[:M] = 0
-
 
     #
     ei_matrix = scipy.sparse.eye_array(N, N, format='csc')
@@ -75,4 +79,45 @@ def l1_method(basis_signal: np.ndarray, sampled_signal: np.ndarray, imgsize: tup
     result = scipy.optimize.milp(c=c[:, 0], constraints=constraints)
     img = np.reshape(result.x[:M], newshape=imgsize)
     residue = result.x[M:]
+    return img.T, residue
+
+
+def IRLSCG(A, B, maxiter, xguess, lbd=10, tolLower=1e-2, epsilon=.01):
+    '''
+		Itera no maximo maxiter vezes o IRLSCG.
+			Lembrando, queremos estimar a solução para A = Bx.
+			A <= A
+			B <= B
+			xguess <= o chute inicial para x
+			lbd <= lambda (deve estar de acordo com a curva L)
+			tolLower <= parar o loop de iterações quando o erro é menor que este valor
+			epsilon <= valor que ajuda na aproximação f(x)=x para f(x) = f1(x)=sqrt(x^2+epsilon)
+			com o objetivo de tornar f(x) diferenciavel
+		'''
+    N = A.shape[1]
+    W = np.zeros(shape=(N, N))
+
+    f = np.zeros(N)
+    f0 = np.sqrt(xguess ** 2 + epsilon)
+    #a = conjgrad(H.T@H+d*W, H.T@g, f[:,0])
+    minTol = np.zeros(maxiter + 1)
+    f1 = None
+
+    for k in range(maxiter):
+        W = np.diag(f0 ** (-1))
+        f1 = linalg.lsqr(A.T @ A + lbd * W, A.T @ B)[0]
+        ek = (np.linalg.norm(f1 - f0, 2) / np.linalg.norm(f0, 2)) ** 2
+        f0 = np.sqrt(f1 ** 2 + epsilon)
+        if (ek < tolLower):
+            return f0, B - A @ f0
+    print("Not converged.")
+    return f1, B - A @ f1
+
+
+def irls_method(basis_signal: np.ndarray, sampled_signal: np.ndarray, imgsize: tuple, damp=0):
+    A = basis_signal
+    b = sampled_signal
+    xguess = linalg.lsqr(A, b)[0]
+    x, residue = IRLSCG(A, b, maxiter=100, xguess=xguess)
+    img = np.reshape(x, shape=imgsize)
     return img.T, residue
